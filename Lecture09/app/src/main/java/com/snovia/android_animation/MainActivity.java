1815d6044d5d0a2dd25207007282b72cc0460dcd
Snovia2398 <snovia2398@gmail.com>
2021-03-27 23:26:37 +0500

    Table Display

--- Lecture09/app/src/main/java/com/snovia/android_animation/MainActivity.java
+++ Lecture09/app/src/main/java/com/snovia/android_animation/MainActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.SeekBar;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,12 +17,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SeekBar seekBar = findViewById(R.id.seekBar);
-        seekBar.setMax(100);
+        ListView listView= findViewById(R.id.ListViewSeekBar);
+        seekBar.setMax(50);
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int index, boolean fromUser) {
-                Log.i("Seekbar value is:",String.valueOf(index));
+                //Log.i("Seekbar value is:",String.valueOf(index));
+                int tableNumber= index;
+                ArrayList<String> tableArrayList= new ArrayList<String>();
+                for(int x=1; x<11; x++){
+                   tableArrayList.add(Integer.toString(x*index));
+                }
+                ArrayAdapter<String> arrayAdapter= new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1,tableArrayList);
+                listView.setAdapter(arrayAdapter);
             }
 
             @Override
