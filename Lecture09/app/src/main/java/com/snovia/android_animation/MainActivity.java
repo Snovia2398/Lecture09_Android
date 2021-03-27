54b9e645a812223abb806ca59a27f8ddd9d9f415
Snovia2398 <snovia2398@gmail.com>
2021-03-28 00:07:18 +0500

    Fade image

--- Lecture09/app/src/main/java/com/snovia/android_animation/MainActivity.java
+++ Lecture09/app/src/main/java/com/snovia/android_animation/MainActivity.java
@@ -1,12 +1,9 @@
 package com.snovia.android_animation;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.SeekBar;
+import android.view.View;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 
@@ -16,32 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SeekBar seekBar = findViewById(R.id.seekBar);
-        ListView listView= findViewById(R.id.ListViewSeekBar);
-        seekBar.setMax(50);
+    }
 
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int index, boolean fromUser) {
-                //Log.i("Seekbar value is:",String.valueOf(index));
-                int tableNumber= index;
-                ArrayList<String> tableArrayList= new ArrayList<String>();
-                for(int x=1; x<11; x++){
-                   tableArrayList.add(Integer.toString(x*index));
-                }
-                ArrayAdapter<String> arrayAdapter= new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1,tableArrayList);
-                listView.setAdapter(arrayAdapter);
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
+    public void Animation(View view) {
+        ImageView imageView= findViewById(R.id.imageView);
+        imageView.animate().alpha(0).setDuration(2000);
     }
 }
